ce9d45fdc08f59f8286f438729ad868bba79cc31
LeonHardt <leonhardt427@126.com>
2017-01-03 16:30:05 +0800

    The answer of 0000

--- MyAnswers/0000/PicNumber.py
+++ MyAnswers/0000/PicNumber.py
@@ -1,1 +1,32 @@
-/****第 0000 题：**将你的 QQ 头像（或者微博头像）右上角加上红色的数字，类似于微信未读信息数量那种提示效果。类似于图中效果 ****/
+#!usr/bin/python
+#-*-coding: utf-8 -*-
+
+
+##第 0000 题：**将你的 QQ 头像（或者微博头像）右上角加上红色的数字，类似于微信未读信息数量那种提示效果。
+
+from PIL import Image, ImageDraw, ImageFont
+
+im = Image.open('Test.jpg')
+print(im.format,im.size, im.mode)
+
+draw = ImageDraw.Draw(im)
+##draw.text((0,0), "4", fill = (255,0,0))
+
+text = "4"
+Font1 = ImageFont.truetype("SFCompactText-Bold.otf" , 20)
+
+
+draw.text([112,0],text ,font = Font1)
+
+
+
+
+
+im.show()
+
+
+
+
+
+
+
