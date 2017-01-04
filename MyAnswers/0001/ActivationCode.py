a5d0ab4528dccdca0f24759b9a18d3da2d91631f
LeonHardt <leonhardt427@126.com>
2017-01-04 17:05:07 +0800

    The answer of 0001(My answer)

---
+++ MyAnswers/0001/ActivationCode.py
@@ -0,0 +1,35 @@
+#!usr/bin/python
+# -*-coding:utf-8 -*-
+
+
+# 第 0001 题：**做为 Apple Store App 独立开发者，你要搞限时促销，为你的应用**生成激活码**（或者优惠券)
+# 使用 Python 如何生成 200 个激活码（或者优惠券）？
+
+import random
+import string
+
+source_code = string.ascii_uppercase + string.digits   #邀请码为26个英文大写字母和0-9十个数字随机组合而成
+def activation_code(number = 10 , length = 10):
+    code_list = {}
+    code = ""
+    for count in range(number):
+        key_name = "key"+ str(count)
+        for num in range(length):
+            code = code + random.choice(source_code)
+        code_list[key_name] = code
+        code = ""
+    return code_list
+
+c = activation_code()
+for key in c :
+    print("C[%s]="% key, c[key])
+
+print(c)
+
+
+
+
+
+
+
+
