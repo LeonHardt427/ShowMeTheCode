cabd626255a0d88fee3c774d1bb0bff867860191
LeonHardt <leonhardt427@126.com>
2017-01-05 13:47:43 +0800

    The full answer of 0005 to achieve batch process

--- MyAnswers/0005/picture_of_ip5.py
+++ MyAnswers/0005/picture_of_ip5.py
@@ -8,18 +8,28 @@
 
 import glob                 # 作为通配符自动打开文件
 from PIL import Image       # 修改图片属性
+import os
 
 
 '''
   iphone5分辨率为1136x640
 '''
 
-def picture_resolution(im):
+def picture_resolution(im, picture_name):
     print(im.size)
-    im.thumbnail((100, 100))
-    im.save('thumb.jpg', 'JPEG')
+    im.thumbnail((1136, 640))
+    im.save(picture_name+'thumb.jpg', 'JPEG')
+
+def pic_basename(pic_full_name):           # 命名中使用原来的名字
+    return pic_full_name.split(".")[0]
 
 
-im_picture = Image.open('test.png')
-picture_resolution(im_picture)
+path = "/Users/leonhart/Documents/Git/TestPicture"
+picture_path = glob.glob(path + '/*.jpg')
+print(picture_path)
+
+for pic in picture_path:
+    pic_name = os.path.basename(pic)
+    im_picture = Image.open(pic)
+    picture_resolution(im_picture, pic_name)
 
